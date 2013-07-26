fbe710d080828c6f30351b44f8b84314e3cad10a
Roflgod <roflxgod@gmail.com>
2013-07-26 15:09:23 -0400

    Updated Timer class with pausing.

--- util/Timer.java
+++ util/Timer.java
@@ -1,47 +1,110 @@
 package com.infibot.client.api.util;
 
-import com.infibot.client.utilities.time.Time;
-
 /**
- * All long variables specified in this class are in milliseconds.
+ * @author Roflgod
  */
-public class Timer
-{
-    private long end;
-    private final long period;
-    private final long start;
-    public Timer(long l) {
-        start = System.currentTimeMillis();
-        period = l;
-        end = period + start;
-    }
+public class Timer {
 
-    public long setEndIn(long a) {
-        return (end = System.currentTimeMillis() + a);
-    }
+	private final long period;
 
-    public String toElapsedString() {
-        return Time.format(getElapsed());
-    }
+	private long start;
+	private long end;
 
-    public String toRemainingString() {
-        return Time.format(getRemaining());
-    }
+	private boolean isPaused = false;
+	private long pauseTime;
 
-    public void reset() {
-        end = System.currentTimeMillis() + period;
-    }
+	public Timer(final long period) {
+		this.period = period;
+		this.start = System.currentTimeMillis();
+		this.end = start + period;
+	}
 
-    public long getElapsed() {
-        return System.currentTimeMillis() - start;
-    }
+	private long getCurrent() {
+		return isPaused ? pauseTime : System.currentTimeMillis();
+	}
 
-    public long getRemaining() {
-        return isRunning() ? end - System.currentTimeMillis() : 0;
-    }
+	public long getElapsed() {
+		return getCurrent() - start;
+	}
 
-    public boolean isRunning() {
-        return System.currentTimeMillis() <= end;
-    }
+	public long getRemaining() {
+		return isRunning() ? end - getCurrent() : 0;
+	}
+
+	public boolean isRunning() {
+		return getCurrent() < end;
+	}
+
+	public void pause() {
+		if (!isPaused) {
+			pauseTime = System.currentTimeMillis();
+			isPaused = true;
+		}
+	}
+
+	public void resume() {
+		if (isPaused) {
+			start = System.currentTimeMillis();
+			long timeLeft = end - pauseTime;
+			end = start + timeLeft;
+			isPaused = false;
+		}
+	}
+
+	public void reset() {
+		start = System.currentTimeMillis();
+		end = start + period;
+	}
+
+	public long setEndIn(long millis) {
+		end = System.currentTimeMillis() + millis;
+		return end;
+	}
+
+	public String toElapsedString() {
+		return format(getElapsed());
+	}
+
+	public String toRemainingString() {
+		return format(getRemaining());
+	}
+
+	private static final int MILLIS_PER_SEC = 1000;
+	private static final int MILLIS_PER_MIN = MILLIS_PER_SEC * 60;
+	private static final int MILLIS_PER_HOUR = MILLIS_PER_MIN * 60;
+	private static final int MILLIS_PER_DAY = MILLIS_PER_HOUR * 24;
+
+	public static String format(long time) {
+		final StringBuilder s = new StringBuilder();
+		final long days = time / MILLIS_PER_DAY;
+		time -= days * MILLIS_PER_DAY;
+		final long hours = time / MILLIS_PER_HOUR;
+		time -= hours * MILLIS_PER_HOUR;
+		final long mins = time / MILLIS_PER_MIN;
+		time -= mins * MILLIS_PER_MIN;
+		final long secs = time / MILLIS_PER_SEC;
+		time -= secs * MILLIS_PER_SEC;
+
+		if (days > 0) {
+			s.append(days);
+			s.append(':');
+		}
+		if (hours < 10) {
+			s.append(0);
+		}
+		s.append(hours);
+		s.append(':');
+		if (mins < 10) {
+			s.append(0);
+		}
+		s.append(mins);
+		s.append(':');
+		if (secs < 10) {
+			s.append(0);
+		}
+		s.append(secs);
+
+		return s.toString();
+	}
 
 }
